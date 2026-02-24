dd1e839af50865f8b4c6e2739fa99c55e3034e25
arshiya-mohamedazeem <Arshiya.Mohamedazeem@aroopatech.com>
2026-02-24 21:28:38 +0530

    add: login API with JWT authentication

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
@@ -27,6 +28,34 @@ exports.register = async (req, res) => {
       userId: user._id,
     });
   } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const isMatch = await bcrypt.compare(password, user.passwordHash);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({
+      message: "Login successful",
+      token,
+    });
+  } catch (err){
     console.error(err);
     res.status(500).json({ message: "Server error" });
   }
